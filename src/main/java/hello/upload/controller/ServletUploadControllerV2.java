29e45c1d7ff61d32671f87ff12526f8c936ff06c
hyos <hyozzang99@gmail.com>
2025-08-04 08:20:52 +0900

    Feat: 파일 업로드와 업로드 된 결과 view로 확인 및 올린 파일 다운로드 실습

--- src/main/java/hello/upload/controller/ServletUploadControllerV2.java
+++ src/main/java/hello/upload/controller/ServletUploadControllerV2.java
@@ -16,46 +16,55 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
+
 @Slf4j
 @Controller
-@RequestMapping("/servlet/v2")public class ServletUploadControllerV2 {
+@RequestMapping("/servlet/v2")
+public class ServletUploadControllerV2 {
+
     @Value("${file.dir}")
     private String fileDir;
+
+
     @GetMapping("/upload")
     public String newFile() {
         return "upload-form";
     }
     @PostMapping("/upload")
-    public String saveFileV1(HttpServletRequest request) throws
-            ServletException, IOException {
+    public String saveFileV1(HttpServletRequest request) throws ServletException, IOException {
         log.info("request={}", request);
+
         String itemName = request.getParameter("itemName");
         log.info("itemName={}", itemName);
+
         Collection<Part> parts = request.getParts();
         log.info("parts={}", parts);
+
         for (Part part : parts) {
             log.info("==== PART ====");
             log.info("name={}", part.getName());
             Collection<String> headerNames = part.getHeaderNames();
             for (String headerName : headerNames) {
-                log.info("header {}: {}", headerName,
-                        part.getHeader(headerName));
+                log.info("header {}: {}", headerName, part.getHeader(headerName));
             }
             //편의 메서드
             //content-disposition; filename
-            log.info("submittedFileName={}", part.getSubmittedFileName());
-            log.info("size={}", part.getSize()); //part body size
+            log.info("submittedFilename={}", part.getSubmittedFileName());
+            log.info("size={}", part.getSize());
+
             //데이터 읽기
             InputStream inputStream = part.getInputStream();
-            String body = StreamUtils.copyToString(inputStream,
-                    StandardCharsets.UTF_8);
-            log.info("body={}", body);
+            String body = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+//            log.info("body={}", body);
+
             //파일에 저장하기
             if (StringUtils.hasText(part.getSubmittedFileName())) {
-                String fullPath = fileDir + part.getSubmittedFileName(); log.info("파일 저장 fullPath={}", fullPath);
+                String fullPath = fileDir + part.getSubmittedFileName();
+                log.info("파일 저장 fullPath={}", fullPath);
                 part.write(fullPath);
             }
         }
+
         return "upload-form";
     }
 }
--- src/main/java/hello/upload/controller/SpringUploadController.java
+++ src/main/java/hello/upload/controller/SpringUploadController.java
@@ -17,24 +17,28 @@ import java.io.IOException;
 @Controller
 @RequestMapping("/spring")
 public class SpringUploadController {
+
     @Value("${file.dir}")
     private String fileDir;
+
     @GetMapping("/upload")
     public String newFile() {
         return "upload-form";
     }
+
     @PostMapping("/upload")
     public String saveFile(@RequestParam String itemName,
-                           @RequestParam MultipartFile file, HttpServletRequest
-                                   request) throws IOException {
+                           @RequestParam MultipartFile file, HttpServletRequest request) throws IOException {
         log.info("request={}", request);
         log.info("itemName={}", itemName);
         log.info("multipartFile={}", file);
+
         if (!file.isEmpty()) {
             String fullPath = fileDir + file.getOriginalFilename();
             log.info("파일 저장 fullPath={}", fullPath);
             file.transferTo(new File(fullPath));
         }
+
         return "upload-form";
     }
 }
